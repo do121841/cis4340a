1bc3c6b2ab72f7b32bd48b347bbad453aa40f8cf
do121841 <do121841@ucf.edu>
2025-03-05 21:43:26 -0500

    New compliant code
    The compliant versions of the five mandated and five student selected rules

--- R16_ENV06_J.java
+++ R16_ENV06_J.java
@@ -1,9 +1,4 @@
 class Stuff {
   private static final bool DEBUG = False;
   // Other fields and methods
-  public static void main(String args[]) {
-    Stuff.DEBUG = True;
-    Stuff stuff = new Stuff();
-    // Test stuff
-  }
 }
